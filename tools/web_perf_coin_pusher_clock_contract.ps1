b044684e0a781a80f8f81e4494c0dac1311543f0
erikjpet <erikjpet@gmail.com>
2026-08-27 04:45:12 -0500

    test(web-perf): bind pusher evidence fail closed

--- tools/web_perf_coin_pusher_clock_contract.ps1
+++ tools/web_perf_coin_pusher_clock_contract.ps1
@@ -9,6 +9,8 @@ function Test-CoinPusherReinstallClockObservation {
         -and [int]$Observation.boundary_tray_count -eq 0 `
         -and [int]$Observation.liveness_after -gt [int]$Observation.liveness_before `
         -and [int]$Observation.observed_body_count -gt 0 `
+        -and [int]$Observation.observed_body_count -eq [int]$Observation.conservation.active `
+        -and [int]$Observation.observed_tray_count -eq [int]$Observation.conservation.tray `
         -and (Test-CoinPusherConservationSnapshot -Snapshot $Observation.conservation -ExpectedOrigin 300)
 }
 
@@ -19,6 +21,7 @@ function Test-CoinPusherConservationSnapshot {
         -and [int]$Snapshot.accounted -eq $accounted `
         -and $accounted -eq $ExpectedOrigin `
         -and [bool]$Snapshot.conservation_ok `
+        -and [bool]$Snapshot.solver_invariants_present `
         -and [bool]$Snapshot.solver_conservation_ok
 }
 
@@ -26,7 +29,12 @@ function Test-CoinPusherReducedSampleBoundary {
     param([object]$Fixture, [object]$Boundary, [object]$ScenarioTags)
     return [int]$Fixture.body_count -eq 300 `
         -and [int]$Boundary.body_count -eq [int]$ScenarioTags.body_count_before `
-        -and [int]$Boundary.tray_count -eq [int]$ScenarioTags.conservation_before.tray `
+        -and [int]$Boundary.body_count -eq [int]$Boundary.conservation.active `
+        -and [int]$Boundary.tray_count -eq [int]$ScenarioTags.tray_count_before `
+        -and [int]$Boundary.tray_count -eq [int]$Boundary.conservation.tray `
+        -and [int]$Boundary.liveness_ticks -eq [int]$ScenarioTags.solver_liveness_before `
+        -and [int]$ScenarioTags.body_count_before -eq [int]$ScenarioTags.conservation_before.active `
+        -and [int]$ScenarioTags.tray_count_before -eq [int]$ScenarioTags.conservation_before.tray `
         -and (Test-CoinPusherConservationSnapshot -Snapshot $Boundary.conservation -ExpectedOrigin 300) `
         -and (Test-CoinPusherConservationSnapshot -Snapshot $ScenarioTags.conservation_before -ExpectedOrigin 300)
 }
@@ -41,6 +49,10 @@ function Get-CoinPusherPostCollectAccounting {
         valid = [int]$Tags.tray_count_at_accept -eq 0 `
             -and [int]$Tags.tray_value_at_accept -eq 0 `
             -and [int]$Tags.body_count_at_accept -gt 0 `
+            -and [int]$Tags.body_count_at_accept -eq [int]$Tags.conservation_at_accept.active `
+            -and [int]$Tags.tray_count_at_accept -eq [int]$Tags.conservation_at_accept.tray `
+            -and [int]$Tags.body_count_after -eq [int]$Tags.conservation_after.active `
+            -and [int]$Tags.tray_count_after -eq [int]$Tags.conservation_after.tray `
             -and $bodyDelta -ge 0 `
             -and $trayDelta -ge 0 `
             -and $trayDelta -le $bodyDelta `
